067d6919967452156388754b14835cd9c673f674
yuuu <okazaki@fusic.co.jp>
2023-05-27 13:03:25 +0900

    Add host that has images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     domains: [
       "files.speakerdeck.com",
       "portfoliov32d439ad14dba4a4c80dff6b447fdbd03162006-dev.s3.ap-northeast-1.amazonaws.com",
+      "portfoliov32d439ad14dba4a4c80dff6b447fdbd0391405-prd.s3.ap-northeast-1.amazonaws.com",
       "qiita-user-contents.imgix.net",
       "tech.fusic.co.jp",
       "res.cloudinary.com",
